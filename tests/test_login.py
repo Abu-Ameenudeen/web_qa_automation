60259fdab6a9074cdcad5386587a32f2443da6b3
Ameen <abuameen.student@gmail.com>
2025-07-07 17:11:10 +0530

    Refactor: use pytest fixture for driver setup and teardown

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,10 @@
-# tests/test_login.py
-
-from utils.driver_factory import get_driver
 from pages.login_page import LoginPage
 import time
 
-def test_login_success():
-    driver = get_driver()
+def test_login_success(driver):  # driver comes from fixture
     login_page = LoginPage(driver)
-
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
 
     time.sleep(2)
     assert "inventory" in driver.current_url
-    driver.quit()
--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,23 +1,15 @@
-# tests/test_logout.py
-
-from utils.driver_factory import get_driver
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 import time
 
-def test_logout():
-    driver = get_driver()
-
-    # Login first
+def test_logout(driver):  # driver from fixture
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
 
-    # Then logout
     time.sleep(2)
     inventory_page = InventoryPage(driver)
     inventory_page.logout()
 
     time.sleep(1)
     assert "saucedemo.com" in driver.current_url and "login" in driver.page_source
-    driver.quit()
