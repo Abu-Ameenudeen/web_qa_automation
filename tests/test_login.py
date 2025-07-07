a6da90222ecd4176cd7673d4d97f35b14966d3ad
Ameen <abuameen.student@gmail.com>
2025-07-07 16:41:15 +0530

    Refactor: implemented Page Object Model for login page

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,16 @@
 # tests/test_login.py
 
 from utils.driver_factory import get_driver
+from pages.login_page import LoginPage
 import time
 
 def test_login_success():
     driver = get_driver()
-    driver.get("https://www.saucedemo.com")
+    login_page = LoginPage(driver)
 
-    # Input credentials
-    driver.find_element("id", "user-name").send_keys("standard_user")
-    driver.find_element("id", "password").send_keys("secret_sauce")
-    driver.find_element("id", "login-button").click()
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
 
-    # Wait and check
     time.sleep(2)
     assert "inventory" in driver.current_url
-
     driver.quit()
