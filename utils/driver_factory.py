a25d5e7af57623a8d2ff03659a5b3a0b250ef06c
Ameen <abuameen.student@gmail.com>
2025-07-07 17:39:52 +0530

    Fix: disable Chrome password popups completely for stable test runs

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -7,12 +7,15 @@ def get_driver():
     service = Service(executable_path=driver_path)
     options = webdriver.ChromeOptions()
 
-    # Suppress the password manager popup
+    # Full suppression of save password popups
     prefs = {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False
     }
+
     options.add_experimental_option("prefs", prefs)
+    options.add_argument("--incognito")
+    options.add_argument("--disable-popup-blocking")
     options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(service=service, options=options)
